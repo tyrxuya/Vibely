7d1fe25c223577707616d42d8b885ea571287000
vankata <ivan2003444@gmail.com>
2025-04-05 16:35:51 +0300

    [MA-010] Added models. Added db sets in the db context

---
+++ Vibely_App/Data/Models/UserPlaylist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vibely_App.Data.Models
+{
+    [Table("users_playlists")]
+    public class UserPlaylist
+    {
+        [Required]
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        [Column("playlist_id")]
+        public int PlaylistId { get; set; }
+        public Playlist Playlist { get; set; }
+    }
+}
