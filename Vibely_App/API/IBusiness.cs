e7366e2e67450a688d1f58d8b351128a4c949f3c
vankata <ivan2003444@gmail.com>
2025-04-05 16:35:13 +0300

    [MA-010] Moved business interfaces to new module - API

---
+++ Vibely_App/API/IBusiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vibely_App.API
+{
+    public interface IBusiness<T>
+    {
+        List<T> GetAll();
+        void Add(T t);
+        T? Find(int id);
+        void Update(int id, T t);
+        void Remove(int id);
+    }
+}
