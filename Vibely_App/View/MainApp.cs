6d4114fadb2a739f58e9d87369295055f9941e4a
vankata <ivan2003444@gmail.com>
2025-04-06 14:21:50 +0300

    [MA-013] Created MainApp form

---
+++ Vibely_App/View/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vibely_App.View
+{
+    public partial class MainApp : Form
+    {
+        public MainApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
