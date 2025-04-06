24fc0bfb4e25876730ee7fb79f8d8c0ec37c575a
Polina <polina.kr.0711@gmail.com>
2025-04-06 23:00:14 +0300

    [MA-009] Added custom user controls.

---
+++ Vibely_App/Controls/GradientPanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vibely_App.Controls
+{
+    public class GradientPanel : Panel
+    {
+        public Color TopColor { get; set; }
+        public Color BottomColor { get; set; }
+        public float Angle { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush brush = new LinearGradientBrush(ClientRectangle, TopColor, BottomColor, Angle);
+            Graphics g = e.Graphics;
+            g.FillRectangle(brush, ClientRectangle);
+            base.OnPaint(e);
+        }
+
+
+    }
+}
