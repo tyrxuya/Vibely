65be62d6d882bfc48b5094e19cca2ce62a07bb25
vankata <ivan2003444@gmail.com>
2025-04-20 00:19:34 +0300

    [MA-011] Implemented search user by credentials operation in business layer

--- Vibely_App/Business/UserBusiness.cs
+++ Vibely_App/Business/UserBusiness.cs
@@ -71,5 +71,14 @@ namespace Vibely_App.Business
                 _dbContext.SaveChanges();
             }
         }
+
+        User? IUserBusiness.FindByCredentials(string username, string password)
+        {
+            IPasswordHasher passwordHasher = new PasswordHasher();
+            password = passwordHasher.Hash(password);
+
+            return _dbContext.Users.Where(u => u.Username == username && u.Password == password)
+                .FirstOrDefault();
+        }
     }
 }
