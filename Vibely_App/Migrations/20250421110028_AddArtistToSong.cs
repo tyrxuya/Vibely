d53b339bbe8336d4490760e6d33b1862f038f146
vankata <ivan2003444@gmail.com>
2025-04-21 14:34:54 +0300

    [MA-015] Migrate database

---
+++ Vibely_App/Migrations/20250421110028_AddArtistToSong.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vibely_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArtistToSong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "artist",
+                table: "songs",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "artist",
+                table: "songs");
+        }
+    }
+}
