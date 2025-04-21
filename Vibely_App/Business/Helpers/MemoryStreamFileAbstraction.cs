6e69c718d0913547c0ad3693b3f8f53fa4d7ed63
vankata <ivan2003444@gmail.com>
2025-04-21 14:34:29 +0300

    [MA-015] Add file abstraction helper class for TagLib# library

---
+++ Vibely_App/Business/Helpers/MemoryStreamFileAbstraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vibely_App.Business.Helpers
+{
+    public class MemoryStreamFileAbstraction : TagLib.File.IFileAbstraction
+    {
+        public string Name { get; }
+        public Stream ReadStream { get; }
+        public Stream WriteStream => throw new NotSupportedException(); // We only need to read
+
+        public MemoryStreamFileAbstraction(string name, Stream stream)
+        {
+            Name = name;
+            ReadStream = stream;
+        }
+
+        public void CloseStream(Stream stream)
+        {
+            stream?.Dispose(); // Dispose the stream when TagLib# is done
+        }
+    }
+
+}
